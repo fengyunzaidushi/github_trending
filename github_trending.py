27906a79628e04832ba3aaa10fc16a6060ce17fe
fengyunzaidushi <fengxiaoyang163@163.com>
2024-02-02 13:02:05 +0800

    add cp data

--- github_trending.py
+++ github_trending.py
@@ -218,7 +218,7 @@ async def main():
     urls.extend(urls2)
     urls.extend(urls3)
     date = get_date()
-    for url in urls[:]:
+    for url in urls[:1]:
         print(url)
         separators = "/="
         split_text = re.split('[{}]'.format(separators), url)
