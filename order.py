64a6c2cf467115f7c43528e8fa4efb1e448691e8
fengyunzaidushi <fengxiaoyang163@163.com>
2024-05-21 15:03:45 +0800

    change order

--- order.py
+++ order.py
@@ -11,8 +11,9 @@ def read_jsonl(file_path):
 if __name__ == '__main__':
 
     #遍历文件夹下所有文件
-    directory = '/mnt/sda/github/02yue/github_daily/data/github'
+    directory = r'E:\share\github\02yue\github_daily\data\github0506'
     for root, dirs, files in os.walk(directory):
+        total_dict = {}
         for file in files:
             if file.endswith('_list.jsonl'):
                 # 读取jsonl文件
@@ -20,7 +21,7 @@ if __name__ == '__main__':
                 data = read_jsonl(file_path)
                 print(len(data))
                 # print(data[0])
-                all_dic = {}
+                file_dic = {}
                 for item in data[:]:
                     dic = json.loads(item)
 
@@ -30,22 +31,38 @@ if __name__ == '__main__':
                         # 三元表达式判断num是否为空
                         repo['stars'] = int(num) if num else 0
         
-                        if repo['name'] not in all_dic:
-                            all_dic[repo['name']] = repo
+                        if repo['name'] not in file_dic:
+                            file_dic[repo['name']] = repo
+                            total_dict[repo['name']] = repo
                         else:
-                            if repo['stars'] > all_dic[repo['name']]['stars']:
-                                all_dic[repo['name']] = repo
+                            if repo['stars'] > file_dic[repo['name']]['stars']:
+                                file_dic[repo['name']] = repo
+                                total_dict[repo['name']] = repo
                 
                 # 字典根据键stars排序   
-                all_dic = dict(sorted(all_dic.items(), key=lambda x: x[1]['stars'], reverse=True))
+                # file_dic = dict(sorted(file_dic.items(), key=lambda x: x[1]['stars'], reverse=True))
 
                 # 字典写入jsonl文件
-                with open(f'./data/order/jsonl/{file}.jsonl', 'w', encoding='utf-8') as f:
-                    json.dump(all_dic, f, ensure_ascii=False, indent=4)
+                # with open(f'./data/order/jsonl/{file}.jsonl', 'w', encoding='utf-8') as f:
+                #     json.dump(file_dic, f, ensure_ascii=False, indent=4)
+                #
+                # # 字典写入json文件,中文不乱码
+                # with open(f'./data/order/merge/{file}_order.json', 'w', encoding='utf-8') as f:
+                #     for item in file_dic.values():
+                #         f.write('    ')
+                #         json.dump(item, f, ensure_ascii=False)
+                #         f.write('\n')
 
-                # 字典写入json文件,中文不乱码
-                with open(f'./data/order/merge/{file}_order.json', 'w', encoding='utf-8') as f:
-                    for item in all_dic.values():
-                        f.write('    ')
-                        json.dump(item, f, ensure_ascii=False)
-                        f.write('\n')
+        # 字典根据键stars排序
+        total_dict = dict(sorted(total_dict.items(), key=lambda x: x[1]['stars'], reverse=True))
+
+        # 字典写入jsonl文件
+        with open(f'./data/order/total/total.jsonl', 'w', encoding='utf-8') as f:
+            json.dump(total_dict, f, ensure_ascii=False, indent=4)
+
+        # 字典写入json文件,中文不乱码
+        with open(f'./data/order/total/total_order.json', 'w', encoding='utf-8') as f:
+            for item in total_dict.values():
+                f.write(str(item['stars']))
+                json.dump(item, f, ensure_ascii=False)
+                f.write('\n')
