8aa0baa1f15f864db4e0623b3706f42a1d6d077c
fengyunzaidushi <fengxiaoyang163@163.com>
2024-06-03 22:32:37 +0800

    add divide

--- order.py
+++ order.py
@@ -1,6 +1,6 @@
 import json
 import os
-
+import time
 # 读取jsonl文件
 def read_jsonl(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
@@ -60,12 +60,41 @@ if __name__ == '__main__':
     total_dict = dict(sorted(total_dict.items(), key=lambda x: x[1]['stars'], reverse=True))
     print(len(total_dict))
     # 字典写入jsonl文件
-    with open(f'./data/order/total/total0603.jsonl', 'w', encoding='utf-8') as f:
-        json.dump(total_dict, f, ensure_ascii=False, indent=4)
+    # with open(f'./data/order/total/total0603.jsonl', 'w', encoding='utf-8') as f:
+    #     json.dump(total_dict, f, ensure_ascii=False, indent=4)
 
-    # 字典写入json文件,中文不乱码
-    with open(f'./data/order/total/total_order0603.json', 'w', encoding='utf-8') as f:
-        for item in total_dict.values():
-            f.write(str(item['stars']))
-            json.dump(item, f, ensure_ascii=False)
+    # # 字典写入json文件,中文不乱码
+    # with open(f'./data/order/total/total_order0603.json', 'w', encoding='utf-8') as f:
+    #     for item in total_dict.values():
+    #         f.write(str(item['stars']))
+    #         json.dump(item, f, ensure_ascii=False)
+    #         f.write('\n')
+    
+
+        # Classify content by language
+    classified_data = {}
+    for key, value in total_dict.items():
+        language = value.get("language", "Unknown")
+        if language not in classified_data:
+            classified_data[language] = []
+        classified_data[language].append(value)
+
+    # Write content by language into separate JSONL files
+    file_paths = {}
+    # get time day
+    time_day = time.strftime("%Y%m%d", time.localtime())
+    for language, items in classified_data.items():
+        num = str(len(items)).zfill(4)
+        file_name = f"./data/order/classified/divide/{num}_{language if language else 'Unknown'}.jsonl"
+        with open(file_name, "w", encoding="utf-8") as file:
+            for item in items:
+                json_line = json.dumps(item, ensure_ascii=False)
+                file.write(json_line + "\n")
+        file_paths[language] = file_name
+
+        file_name2 = f"./data/order/classified/merge/{num}_{language if language else 'Unknown'}.jsonl"
+        with open(file_name2, 'a+', encoding='utf-8') as f:
+            sub_dict = {language: items}
+            json.dump(sub_dict, f, ensure_ascii=False, indent=4)
+            # json文件插入一行空格
             f.write('\n')
